344aae29bb1e9493d95e810fea0302e72193f501
Aditya Mishra <adityame222@gmail.com>
2025-09-10 00:03:43 +0530

    update

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -9,8 +9,7 @@ const Video = () => {
         loop
         muted
         playsInline
-        src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1757325210~exp=1757328810~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=d0392644019b2eb36f35a3404dfbf5a8d8a053b5dfb23251086128ef8a30a200&r=dXMtd2VzdDE%3D"
-      >
+        src="https://player.vimeo.com/progressive_redirect/playback/899938964/rendition/1080p/file.mp4?loc=external&log_user=0&signature=6a2cb0320d09d02dcfdfa2ad504c89a510e6d93d55e9060b1b55d680fa2e2be9">
         Your browser does not support the video tag.
       </video>
     </div>
